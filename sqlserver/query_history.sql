f3b9d36a341ef7e163f3a0f3bc5b8ab9cf9619ef
Marcin Szymański <ms32035@gmail.com>
2024-03-23 10:29:17 +0000

    extend query history for sqlserver

--- sqlserver/query_history.sql
+++ sqlserver/query_history.sql
@@ -1,7 +1,32 @@
 SELECT
     deqs.execution_count AS [Count],
     deqs.last_execution_time AS [Time],
-    dest.text AS [Query]
+    dest.text AS [Query],
+    deqs.total_elapsed_time / 100000 as total_elapsed_time,
+    deqs.last_elapsed_time / 100000 as last_elapsed_time,
+    deqs.max_elapsed_time / 100000 as max_elapsed_time,
+    deqs.total_rows,
+    deqs.last_rows,
+    deqs.max_rows,
+    deqs.total_logical_reads,
+    deqs.last_logical_reads,
+    deqs.max_logical_reads,
+    deqs.total_dop,
+    deqs.last_dop,
+    deqs.max_dop,
+    deqs.total_grant_kb,
+    deqs.last_grant_kb,
+    deqs.max_grant_kb,
+    deqs.total_num_physical_reads,
+    deqs.max_num_physical_reads,
+    deqs.last_num_page_server_reads,
+    deqs.total_columnstore_segment_reads,
+    deqs.total_columnstore_segment_skips,
+    deqs.last_columnstore_segment_reads,
+    deqs.last_columnstore_segment_skips,
+    deqs.max_columnstore_segment_reads,
+    deqs.max_columnstore_segment_skips
+
 FROM
     sys.dm_exec_query_stats AS deqs
 CROSS APPLY
